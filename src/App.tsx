0af94e057eac975c6520afb433147f5c9bba90a5
Dev Michoma <codesplint151@gmail.com>
2024-11-09 20:31:45 +0300

    Styling the Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 
 import './index.css'
 
-function App() {
-   
-  return (
-   
- <div>Test</div>
-   
+
+function App() {   
+  return ( 
+  <main className='max-w-2xl mx-auto px-4 py-8' >
+      <h1 className='uppercase text-4xl text-center'> Find the Cheapest BTC</h1> 
+  </main>
   )
 }
-
 export default App
