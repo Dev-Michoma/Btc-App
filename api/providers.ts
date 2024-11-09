bc51cf92d0462d1733253bf09c52dc2ec9b22b69
Dev Michoma <codesplint151@gmail.com>
2024-11-10 01:10:01 +0300

    Working on the Api Module

---
+++ api/providers.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+ export async function getOfferFromPaybis(amount:number):Promise<string | void>{
+
+try{
+    const res = await axios.post('https://api.paybis.com/public/processing/v2/quote/buy-crypto' ,{
+        "currencyCodeFrom":"USD",
+        "currencyCodeTo":"BTC",
+        "requestedAmount":{"amount":amount.toString(),
+        "currencyCode":"USD"},
+        "requestedAmountType":"from",
+        "promoCode":null,
+        "paymentMethod":"credit-card"});
+        return res.data?.paymentMethods?.[0]?.amountTo?.amount.toString();
+} catch (e){
+     console.error(e);
+     return;
+}
+
+  
+
+}
