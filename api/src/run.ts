5fd3149f56d50f42da6f8b52fcd5bf16c87359cb
Dev Michoma <codesplint151@gmail.com>
2024-11-12 00:42:04 +0300

    update

--- api/src/run.ts
+++ api/src/run.ts
@@ -1,11 +1,16 @@
-import { getOfferFromPaybis } from "./provider";
+import { getOfferFromGuardian, getOfferFromMoonPay, getOfferFromPaybis, getOfferFromTransac } from "./provider";
 
 export default async function handleRequest(params: Record<string, any>, context: Record<string, any>) {
     console.log('Received params:', params);
-    const btc = await getOfferFromPaybis(100);
+    const paybis= await getOfferFromPaybis(100);
+    const guardarian = await getOfferFromGuardian(100);
+    const moonpay = await getOfferFromMoonPay(100);
+    const transak = await getOfferFromTransac(100);
     return {
-        message:btc,
+        paybis ,guardarian ,moonpay ,transak ,
     };
 }
 
 handleRequest({ test: 'value' }, {}).then(response => console.log('Response:', response));
+
+// handleRequest({ }, {}).then(response => console.log('Response:', response));
